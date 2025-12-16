468aba144d36f66a23972f1cb444a6999a8923be
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-16 16:15:39 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
+const axios = require('axios')
 const cors = require('cors');
 const app = express();
 app.use(cors());
