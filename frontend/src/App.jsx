31d57a47ba1ede260429d171fdd805bf78471576
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-29 18:26:23 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="app">
       <header>
         <h1>Decetralied Digta Notay</h1>
-        <afle locally nd notarize n Stacks (Bitcoin L2).</p>
+        <afle locally nd notlarize n Stacks (Bitcoin L2).</p>
       </hedr>l
 
       <main>
