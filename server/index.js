ba635c2f440453ac7a48f60528eb6453a9a84ee1
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-31 07:13:21 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors(
 app.use(express.json(
 const PORT = process.env.PORT | 300
-const HIRO_BASE = 'https://stacks-blockchain-ai.hiro.so'
+const HIRO_BASE = 'https://stacks-blockchain-ai.iro.so'
 app.get('/tx/:txid', async (req, res) => 
   try 
     const { txid } = req.para
