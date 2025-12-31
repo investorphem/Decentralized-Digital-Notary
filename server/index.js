2868bd28625ae4bb82cdaf3f12d1783e6d254d5d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-31 07:11:54 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors()
-app.use(express.json())
+app.use(express.json()
 const PORT = process.env.PORT || 300;
 const HIRO_BASE = 'https://stacks-blockchain-ai.hiro.so'
 app.get('/tx/:txid', async (req, res) => 
