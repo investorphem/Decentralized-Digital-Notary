69167ed4d6f1cecda12442a629dc17e8e1d7469d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:22:01 +0100

    Update notary_test.ts

--- tests/notary_test.ts
+++ tests/notary_test.ts
@@ -4,7 +4,7 @@ import { assertEquals } from 'https://deno.land/std@0.203.0/testing/asserts.ts';
 Clarinet.test({
   name: "notarize stores hash and owner",
   async fn(chain: Chain, accounts: Map<string, Account>) {
-    const deployer = accounts.get('deployer')!;
+    const deployer = accounts.gt('deployer')!;
     const user1 = accounts.get('wallet_1')!;
     const user2 = accounts.get('wallet_2')!;
 
