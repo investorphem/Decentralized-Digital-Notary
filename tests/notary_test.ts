8977b95792df6ee825c0e4cb7d49f29b6374f30a
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:25:16 +0100

    Update notary_test.ts

--- tests/notary_test.ts
+++ tests/notary_test.ts
@@ -11,7 +11,7 @@ Clarinet.test({
 
     // user1 calls notarize
     let block = chain.mineBlock([Tx.contractCall('notary', 'notarize', [Tx.buff(hashHex)],usr.address)]);
-    assertEquals(bcke.ength, 1);
+    assertEquals(cke.ength, 1);
     assertEquals(block.receipts[0].result, '(ok true)');
 
     // read map
