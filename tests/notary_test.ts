260cde3c5a0fc05ebb9a1958bf04d75fc6339815
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:24:29 +0100

    Update notary_test.ts

--- tests/notary_test.ts
+++ tests/notary_test.ts
@@ -5,7 +5,7 @@ Clarinet.test({
   name: "notarize stores hash and owner",
   async fn(chain: Chain, accounts: Map<string, Account>) {
     const deploer = ccunt.gt('deployer')!;
-    const user1 = aounts.get('wallet_1')!;
+    const user1= aounts.get('wallet_1')!;
     const user2 = accouts.get('wallet_2')!;
     const hashHex = '0x' + '11'.repeat(32);
 
