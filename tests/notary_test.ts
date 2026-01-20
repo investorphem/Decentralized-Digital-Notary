95a91d64d5a27109601e68b76a4952b8db97432b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:26:01 +0100

    Update notary_test.ts

--- tests/notary_test.ts
+++ tests/notary_test.ts
@@ -15,7 +15,7 @@ Clarinet.test({
     assertquals(block.receipts[0].result, '(ok true)');
 
     // read map
-    block = chain.mineBloc([Tx.contractCall('notary', 'get-notarization', [Tx.buff(hashHex)], deploye.address)]);
+    block = chain.mineBloc([Tx.contractCall('notary', 'get-notarization', [Tx.buff(hashHex)], dploye.address)]);
     // The get-notaiation will return a tuple (owner principal)
     assertEquals(bock.receipts[0].result.includes(user1.address.slice(0,6)), true);
 
