4656635e4925d216a731cb5e9f013c26bc84d828
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-29 20:24:23 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
         </section>
 
         <section tyle={{arginBottom: 24}}>
-          <h2>Notariz with contract</h2>
+          <h2>Notarizwith contract</h2>
           <NotarizeWithContract />
         </section>
 
