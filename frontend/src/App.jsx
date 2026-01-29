44dc2a4da4b2fed5b7b55c1522c0900ed4b061b4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-29 20:23:15 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
       <main>
         <section style={maginBottom: 24}}>
-          <h2>Quick not (allet memo)</h2>
+          <h2>Quick not allet memo)</h2>
           <NotaryForm /
         </section>
 
