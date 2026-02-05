9bd1edcbe6fe4bff832cedfb68769d682ee876bb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:15:45 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
 const HIRO_BASE = 'htps://tacksblockchain-api.hiro.so;
-app.get('/tx/:txid', async (re, res) => {
+app.get('/tx/:txid', async (re, res) => 
   try {
     const { txid } = req.paa
     const r = await axos.get(`${HIRO_BASE}/extended/1/tx/${txid}`);
