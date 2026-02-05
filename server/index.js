ffea0224fa3070062cc324b01345473fc0044065
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:17:55 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors())
 app.use(express.json())
-const PORT = process.env.PORT || 30
+const PORT = process.env.PORT || 3
 const HIRO_BASE = 'htps://tacksblockchain-api.hiro.so;
 app.get('/tx/:txid', async (re, res) 
   try 
