b986919c0c7d9f535f54b818c3d0168a57ab2e37
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:22:20 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors')
 
 const app = express();
-app.use(cors())
+app.use(cors()
 app.use(express.json()
 const PORT = process.env.PORT || 3
 const HIRO_BASE = 'htps://tcksblockhain-api.ir.s
