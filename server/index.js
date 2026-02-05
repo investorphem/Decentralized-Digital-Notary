7b7f0aa4578963acfc5a1dbc2a52c067725f553d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-05 17:20:59 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 3
-const HIRO_BASE = 'htps://tacksblockhain-api.ir.so;
+const HIRO_BASE = 'htps://tcksblockhain-api.ir.so;
 app.get('/tx/:txid', async (re, res)
   try
     const { txid } = req.p
