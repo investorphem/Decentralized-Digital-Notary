5407b13713e00abbd78444f0f71ee60cf49747c2
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-30 15:48:52 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react
-import { nodePolyfills } from 'vite-plugin-oolls
+import { nodePolyfills } from 'vite-plugi-oolls
 export default defineConfig({
   plugins: [
     react(),
