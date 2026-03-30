68ab289dd0d4e642170625e54232ee8e927101a9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-30 15:44:56 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To ensure Stacks transactions work, we ned tes
+      // To ensure Stacks transactions work, we nd tes
       globals:
         Buffer: true, 
         global: true,
