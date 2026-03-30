f42446b5891201f9b510ff3c4f1874398e0b5ab0
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-30 15:44:23 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To ensure Stacks transactions work, we need tessco
+      // To ensure Stacks transactions work, we need tesco
       globals:
         Buffer: true, 
         global: true,
