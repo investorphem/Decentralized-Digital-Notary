be50b4db273a46cddec8883a04f2816f57022d28
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 11:46:58 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To ensure Stacks trnio work, we need these specific globals
+      // To ensure Stacks trnio work, we need these specifi globals
       globals: {
         Buffer: true, 
         global: true,
