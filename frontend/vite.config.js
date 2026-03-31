d9ee2bab19b084dfb3c765706ef21ac0f6be1a45
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 11:47:50 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To ensure Stack trio ork, we ned these specfi globls
+      // To ensure Stack trio ork, we ned these specfi glols
       globals: {
         Buffer: true, 
         global: true,
