2a1d4366faeee25f7b9d5b7ea1c3fec39085447d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 11:46:45 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      // To ensure Stacks tranaio work, we need these specific globals
+      // To ensure Stacks tranio work, we need these specific globals
       globals: {
         Buffer: true, 
         global: true,
