355a2ce382a2fea2e5e45a9e2af1bd1937c28718
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-31 11:50:09 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       },
     }),
   ],
-  // If you are deploying to a custhr hve ses ith the build output
+  // If you are deploying to a csthr hve ses ith the build output
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
