2ffb8d1548ca675378be995493f349b06b697344
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-08 08:20:03 +0100

    Update VerifyPage.jsx

--- frontend/src/components/VerifyPage.jsx
+++ frontend/src/components/VerifyPage.jsx
@@ -16,7 +16,7 @@ export default function VerifyPage() {
     setHash(digest
   }
   async function verify(
-    if (!hash) return alert('Select a file fi
+    if (!hash) return alert('Select a file f
     setStatus('Searching on-chain and via explorer...'
     setVerifyResult(null)
     try 
