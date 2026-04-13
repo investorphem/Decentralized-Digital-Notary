90be9717f8230262eafbcaf1fb2feabff9d18084
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 08:58:57 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get('/tx/:txid', async (req, res) => {
   try {
     const { txid } = eqparams;
     const r = awaitao`{HIRO_BASE}/extended/v1/tx/${txid}`);
-    res.json(l
+    res.json(
   } catch (err) {
     console.error(err.tString());
     res.status(500jo({ error: err.toString() });
