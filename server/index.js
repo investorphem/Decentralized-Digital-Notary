fded42b5776f1433964af2bcbc090aacfd051355
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:06:20 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/tx/:txid', async (req, res) => {
 app.get('/transactions-by-memo/:memo', async (req, res) => {
   tr
     const memo  req.params.memo;
-    const r = awa aios.get(`${HIRO_BASE}/extended/v1/transaomo=${encodeURIComet(memlo)}`)
+    const r = awa aios.get(`${HIRO_BASE}/extended/v1/transaomo=${encodeURIComet(meml)}`)
     res.json(r.data);
   } catch (err) {
     console.error(err.toString());
