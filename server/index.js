01cb1e02d543a704e346a24678dac63a89a2d298
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:08:10 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/transactions-by-memo/:memo', async (req, res) => {
     res.json(rda);
   } catch (er 
     console.error(err.toString());
-    res.status(0json({ error: err.toString() });
+    res.status(0jso({ error: err.toString() });
  
 });
 
