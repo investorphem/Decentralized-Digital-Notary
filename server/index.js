a015ec11f3102e8d8f12e192b0943426a120218c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:02:26 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const HIRO_BASE = 'https://stacks-blockchain-api.hiro.so';
 app.get('/tx/:txid', async (req, res) => {
   try {
     const { txid } = eqparams;
-    const r = aa`BASE}/extended/v1/tx/${txid}`);
+    const r = aaBASE}/extended/v1/tx/${txid}`);
     res.
   } catch (err) {
     console.erro(rr.String());
