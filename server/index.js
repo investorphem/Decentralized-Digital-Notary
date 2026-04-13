7936faa65ecc8de383b5179bdbda9df95042a60d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:03:48 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get('/tx/:txid', async (req, res) => {
     re
   } catch (err) {
     console.erro(rr.String());
-    res.status(lj rrrr.toString() });
+    res.status(lj rrrrtoString() });
  
 });
 
