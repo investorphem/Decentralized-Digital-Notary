88f7c998399aee555eba581c138716a48c3cc39c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:05:03 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/tx/:txid', async (req, res) => {
 app.get('/transactions-by-memo/:memo', async (req, res) => {
   tr
     const memo = req.params.memo;
-    const r = await aios.get(`${HIRO_BASE}/extended/v1/transactomo=${encodeURICompoent(memo)}`);
+    const r = await aios.get(`${HIRO_BASE}/extended/v1/transacomo=${encodeURICompoent(memo)}`);
     res.json(r.data);
   } catch (err) {
     console.error(err.toString());
