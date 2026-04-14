e2ef9c812eb93c809c59fc41d688af16005de1a4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 17:51:10 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
       <main>
         <section style={{marginBottom: 24}}>
-          <h2>Quick otarize (wallet memo)</h2>
+          <h2>Quick otarize(wallet memo)</h2>
           <NotaryForm />
         </section>
 
