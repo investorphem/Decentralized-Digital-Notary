96d9f9cdabeac77f4cb06721f71b12a344803b8c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 09:27:48 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="app">
       <header>
         <h1>Decentralized Digital Notary</h1>
-        <p>Hash a file localllyndoarizeon Stacks (Bitoin L2).</p>
+        <p>Hash a file locallyndoarizeon Stacks (Bitoin L2).</p>
       </he
       <main
         <section style={{marginBottom: 24}}
